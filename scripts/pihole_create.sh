4d52cbd00d84f7bf37e1ebf8fddd0866b1b76f41
abominox <pwnagepal@gmail.com>
2018-02-20 23:38:12 -0500

    Sync commit

--- scripts/pihole_create.sh
+++ scripts/pihole_create.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
+IPv6_LOOKUP="$(ip -6 route get 2001:4860:4860::8888 | awk '{ print $10; exit }')"  # May not work for VPN / tun0
+IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
+IPv6="${IPv6:-$IPv6_LOOKUP}"  # use $IPv6, if set, otherwise IP_LOOKUP
+DOCKER_CONFIGS="$(pwd)"  # Default of directory you run this from, update to where ever.
+
 docker run -d \
 --name pihole \
--p 53:53/tcp -p 53:53/udp -p 80:80 \
--v "pihole:/etc/pihole/" \
--v "pihole:/etc/dnsmasq.d/" \
+-p 53:53/tcp \
+-p 53:53/udp \
+-p 80:80 \
+-v "${DOCKER_CONFIGS}/pihole/:/etc/pihole/" \
+-v "${DOCKER_CONFIGS}/dnsmasq.d/:/etc/dnsmasq.d/" \
 -e ServerIP="${IP}" \
--e ServerIPv6="${IPv6}" \
---restart=always \
+--restart=unless-stopped \
 diginc/pi-hole-multiarch:debian_armhf
--- scripts/vpn_ARM_create.sh
+++ scripts/vpn_ARM_create.sh
@@ -7,3 +7,7 @@ docker run -v vpn:/etc/openvpn --rm giggio/openvpn-arm ovpn_genconfig -u udp://C
 docker run -v vpn:/etc/openvpn --rm -it giggio/openvpn-arm ovpn_initpki nopass
 
 docker run -v --restart=always vpn:/etc/openvpn -d --name openvpn -p 1194:1194/udp --cap-add=NET_ADMIN giggio/openvpn-arm
+
+docker run -v vpn:/etc/openvpn --rm -it giggio/openvpn-arm easyrsa build-client-full CLIENTNAME nopass
+
+docker run -v vpn:/etc/openvpn --rm giggio/openvpn-arm ovpn_getclient CLIENTNAME > CLIENTNAME.ovpn
