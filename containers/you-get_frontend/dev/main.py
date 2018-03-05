68ef59b5ed2036d9571d464cb5f3dabf4e06756a
abominox <pwnagepal@gmail.com>
2018-03-05 15:45:20 -0500

    Remove rutorrent script

--- containers/you-get_frontend/dev/main.py
+++ containers/you-get_frontend/dev/main.py
@@ -1,12 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return """TEST"""
+    return render_template('index.html')
 
-@app.route('/index.html')
-
+#@app.route('/index.html')
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, debug=True)
+    app.run(host='0.0.0.0', use_reloader=True, debug=True)
