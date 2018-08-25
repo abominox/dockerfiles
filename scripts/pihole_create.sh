38c1d02cdaff291140c93ffe3a3ba9e31b1dd232
abominox <pwnagepal@gmail.com>
2018-08-25 06:55:07 -0400

    Init. commit of torrenting stack docker-compose file.

--- scripts/pihole_create.sh
+++ scripts/pihole_create.sh
@@ -3,9 +3,11 @@
 
 docker run -d \
 --name pihole \
--p 53:53/tcp -p 53:53/udp -p 80:80 \
+-p 53:53/tcp \
+-p 53:53/udp \
+-p 8081:80 \
 -v "/mnt/mdrive/misc/docker/pihole/:/etc/pihole/" \
 -v "/mnt/mdrive/misc/docker/pihole/dnsmasq.d/:/etc/dnsmasq.d/" \
--e ServerIP="192.168.1.7" \
+-e ServerIP="192.168.1.3" \
 --restart=always \
 diginc/pi-hole:latest
